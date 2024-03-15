eff29079c9daeb16ce2b747e09c8ecb2b128cf2d
Ngo Thi Kim Ly <ntkl1710@gmail.com>
2024-03-15 17:54:44 +0700

    fetch API

--- fetch.js
+++ fetch.js
@@ -1,27 +1,28 @@
 
-
+document.getElementById('button').style.display = "none";
 fetch('https://fakestoreapi.com/products?limit=10')
     .then(function(response) {
         return response.json();
     })
     .then(function(products) {
-      
+        
         document.getElementById('buttonload').addEventListener("click", function() {
             let firstFiveProducts = products.slice(0, 5);
 
              let htmls = firstFiveProducts.map(function(goods) {
             return `<li>
+                <img src="${goods.image}" alt="${goods.title}">
                 <h2>${goods.title}</h2>
                 <div>${goods.body}</div>
-                <img src="${goods.image}" alt="${goods.title}">
             </li>`;
         });
 
             let html = htmls.join('');
             document.getElementById('container').innerHTML = html;
+            document.getElementById('button').style.display = "block";
 
         });
-   
+        
         document.getElementById('button').addEventListener("click", function() {
             let secondFiveProducts = products.slice(5, 10);
 
